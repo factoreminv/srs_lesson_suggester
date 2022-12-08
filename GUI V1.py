ed7fe5ee73abbab8d0d7fc238b9321b79e5a5ccf
Özgür Soysal <120128014+factoreminv@users.noreply.github.com>
2022-12-09 00:10:38 +0300

    added a basic GUI for schedule search

---
+++ GUI V1.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from main import hour_finder, day_setter, schedule
+
+def show_entry_fields():
+    print(hour_finder(day_setter(schedule(c1.get(), c2.get(), c3.get(), "2022", "1"))))
+
+master = tk.Tk()
+tk.Label(master,
+         text="Department").grid(row=0)
+tk.Label(master,
+         text="Course").grid(row=1)
+tk.Label(master,
+         text="Section").grid(row=2)
+
+c1 = tk.Entry(master)
+c2 = tk.Entry(master)
+c3 = tk.Entry(master)
+
+c1.grid(row=0, column=1)
+c2.grid(row=1, column=1)
+c3.grid(row=2, column=1)
+
+tk.Button(master,
+          text='List', command=show_entry_fields).grid(row=3,
+                                                       column=1,
+                                                       sticky=tk.W,
+                                                       pady=4)
+
+tk.mainloop()
