666b9c71929fb7c208f41a287765c275ac10db57
Özgür Soysal <120128014+factoreminv@users.noreply.github.com>
2022-12-09 20:07:30 +0300

    Reduced the UI to Just selection

--- GUI V1.py
+++ GUI V1.py
@@ -1,42 +1,35 @@
 import tkinter as tk
-from main import hour_finder, day_setter, schedule
 
-def show_entry_fields():
-    try:
-        lis = hour_finder(day_setter(schedule(c1.get(), c2.get(), c3.get(), "2022", "1")))
-        i = 1
-        text_box.delete("1.0", tk.END)
-        for tup in lis:
-            #print(tup[0],tup[1]+" - "+tup[2])
-            text_box.insert(str(i)+".0",tup[0]+" "+tup[1]+" - "+tup[2]+" "+tup[3]+" "+tup[4]+"\n")
-            i += 1
-    except:
-        text_box.delete("1.0", tk.END)
-        text_box.insert("1.0","CLASS NOT FOUND\n")
+department_list = []
 
+def save_department():
+    entry = str(c1.get()).upper()
+    department_list.append(entry)
+    text_box.insert("1.0",entry+"\n")
+
+def class_look():
+    master.destroy()
+    return department_list
 
 master = tk.Tk()
 tk.Label(master,
          text="Department").grid(row=0)
-tk.Label(master,
-         text="Course").grid(row=1)
-tk.Label(master,
-         text="Section").grid(row=2)
 
 c1 = tk.Entry(master)
-c2 = tk.Entry(master)
-c3 = tk.Entry(master)
 
 text_box = tk.Text()
-text_box.grid(row=3,column=1)
+text_box.grid(row=1,column=1)
 
 c1.grid(row=0, column=1)
-c2.grid(row=1, column=1)
-c3.grid(row=2, column=1)
 
 tk.Button(master,
-          text='List', command=show_entry_fields).grid(row=4,
+          text='Add', command=save_department).grid(row=2,
                                                        column=1,
+                                                       sticky=tk.W,
+                                                       pady=4)
+tk.Button(master,
+          text='Display Classes', command=class_look).grid(row=2,
+                                                       column=2,
                                                        sticky=tk.W,
                                                        pady=4)
 
