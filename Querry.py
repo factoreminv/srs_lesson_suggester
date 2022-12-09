9a4a53be9359c110526e6876221ffaa85f254199
Özgür Soysal <120128014+factoreminv@users.noreply.github.com>
2022-12-09 22:37:48 +0300

    added time and date dependency /file support

--- Querry.py
+++ Querry.py
@@ -4,18 +4,31 @@ from datetime import datetime
 def querry(dept):
     class_list =[]
     for n in range(101, 499):
-        try:
-            lis = hour_finder(day_setter(schedule(dept, n, "1", "2022", "1")))
-            now = datetime.now()
-            current_time = now.strftime("%H:%M:%S")
-            current_day = now.strftime("%A")
+        for sect in range(1,20):
+            try:
+                lis = hour_finder(day_setter(schedule(dept, n, sect, "2022", "1")))
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+                current_day = now.strftime("%A")
 
-            for element in lis[0::2]:
-                if element[0] == current_day and element[1] > "13:20":
-                    class_list.append(tuple([dept]) + tuple([str(n)]) + tuple(lis))
-        except:
-            None
+                for element in lis[0::2]:
+                    if element[0] == current_day and element[1] > "13:20":
+                        class_list.append(tuple([dept]) + tuple([str(n)]) +tuple([sect]) + tuple(lis))
+                        print(tuple([dept]) + tuple([str(n)]) +tuple([sect]) + tuple(lis))
+            except:
+                None
     return class_list
 
-
-print(querry("MATH"))
+def create_querry_file(dept):
+    file = open(dept+".txt","w",encoding="utf-8")
+    for n in range(101, 499):
+        for sect in range(1,20):
+            try:
+                lis = hour_finder(day_setter(schedule(dept, n, sect, "2022", "1")))
+                file.write(str(tuple([dept]) + tuple([str(n)]) + tuple([str(sect)]) + tuple(lis))+"\n")
+                print(str(tuple([dept]) + tuple([str(n)]) + tuple([str(sect)]) + tuple(lis)))
+            except:
+                None
+    file.close()
+#querry("PHYS")
+create_querry_file("MATH")
